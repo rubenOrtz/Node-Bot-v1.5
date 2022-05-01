fe3cf5b07bd7e66f62292c46fc18e375655ec34f
LyricalString <lyricalstring@outlook.es>
2022-05-01 17:41:36 +0200

    just testing

--- slash/commands/Diversión/8ball.js
+++ slash/commands/Diversión/8ball.js
@@ -1,79 +1,65 @@
-const { Client, CommandInteraction, MessageEmbed, Discord } = require('discord.js');
+const {
+   Client,
+   CommandInteraction,
+   MessageEmbed,
+   Discord
+} = require('discord.js');
 const Command = require('../../../structures/command.js');
 
 module.exports = class ball extends Command {
-    constructor(client) {
-       super(client, {
-        name: 'ball',
-        description: 'Ask the magic 8ball a question',
-        name_localizations: {
-           'es-ES': 'ball',
-        },
-        description_localizations: {
-           'es-ES': 'Pregunta al poderoso 8ball una pregunta',
-        },
-        cooldown: 5,
-        options: [{
-           type: 3,
-           name: 'question',
-           description: 'The question to ask',
-           name_localizations: {
-              'es-ES': 'pregunta'
-           },
-           description_localizations: {
-              'es-ES': 'La pregunta a preguntar'
-           },
-           required: true
-         }
-        ]
-     });
-    }
-    /**,
-* @param {Client} client
-* @param {CommandInteraction} interaction
-* @param {String[]} args
-*/
-async run(client, interaction, args) {
-    // try {
-        let respuesta = client.language.QUESTIONBALL[4];
-        let argumentos = args.join(" ");
-        if (!argumentos.includes("?")) {
-          const errorembed = new MessageEmbed()
+   constructor(client) {
+      super(client, {
+         name: 'ball',
+         description: 'Ask the magic 8ball a question',
+         name_localizations: {
+            'es-ES': 'ball',
+         },
+         description_localizations: {
+            'es-ES': 'Pregunta al poderoso 8ball una pregunta',
+         },
+         cooldown: 5,
+         options: [{
+            type: 3,
+            name: 'question',
+            description: 'The question to ask',
+            name_localizations: {
+               'es-ES': 'pregunta'
+            },
+            description_localizations: {
+               'es-ES': 'La pregunta a preguntar'
+            },
+            required: true
+         }]
+      });
+   }
+   /**,
+    * @param {Client} client
+    * @param {CommandInteraction} interaction
+    * @param {String[]} args
+    */
+   async run(client, interaction, args) {
+      // try {
+      let respuesta = client.language.QUESTIONBALL[4];
+      let argumentos = args.join(" ");
+      if (!argumentos.includes("?")) {
+         const errorembed = new MessageEmbed()
             .setColor("RED")
             .setTitle(client.language.ERROREMBED)
             .setDescription(
-              client.language.QUESTIONBALL[3]
+               client.language.QUESTIONBALL[3]
             )
             .setFooter(interaction.member.user.username + "#" + interaction.member.user.discriminator, interaction.member.displayAvatarURL());
-          return interaction.editReply({ embeds: [errorembed] })
-        }
-        var random = respuesta[Math.floor(Math.random() * respuesta.length)]; //aqui decimos que va a elegir una respuesta random de el let respuesta
-        const embed = new MessageEmbed() //definimos el embed
-          .addField(client.language.QUESTIONBALL[1], `${args.join(" ")}`) //primer valor decimos a su pregunta y en el segundo valor va la pregunta que iso el usuario
-          .addField(client.language.QUESTIONBALL[2], `${random}`) //primer valor decimos "Mi respuesta" y en el segundo decimos que va a agarrar el var random
-          .setColor("#008822"); //un color random
-        interaction.editReply({ embeds: [embed] }); //y que mande el embed
-    //   } catch (e) {
-    //     console.error(e);
-    //     message.channel.send({
-    //       embeds: [
-    //         new Discord.MessageEmbed()
-    //           .setColor("RED")
-    //           .setTitle(client.language.ERROREMBED)
-    //           .setDescription(client.language.fatal_error)
-    //           .setFooter(message.author.username, message.author.avatarURL())
-    //       ]
-    //     });
-    //     webhookClient.send(
-    //       `Ha habido un error en **${message.guild.name} [ID Server: ${message.guild.id}] [ID Usuario: ${message.author.id}] [Owner: ${message.guild.ownerId}]**. Numero de usuarios: **${message.guild.memberCount}**\nMensaje: ${message.content}\n\nError: ${e}\n\n**------------------------------------**`
-    //     );
-    //     try {
-    //       message.author
-    //         .send(
-    //           "Oops... Ha ocurrido un eror con el comando ejecutado. Aunque ya he notificado a mis desarrolladores del problema, ¿te importaría ir a discord.gg/nodebot y dar más información?\n\nMuchísimas gracias rey <a:corazonmulticolor:836295982768586752>"
-    //         )
-    //         .catch(e);
-    //     } catch (e) { }
-    //   }
- }
+         return interaction.editReply({
+            embeds: [errorembed]
+         })
+      }
+      var random = respuesta[Math.floor(Math.random() * respuesta.length)]; //aqui decimos que va a elegir una respuesta random de el let respuesta
+      const embed = new MessageEmbed() //definimos el embed
+         .addField(client.language.QUESTIONBALL[1], `${args.join(" ")}`) //primer valor decimos a su pregunta y en el segundo valor va la pregunta que iso el usuario
+         .addField(client.language.QUESTIONBALL[2], `${random}`) //primer valor decimos "Mi respuesta" y en el segundo decimos que va a agarrar el var random
+         .setColor("#008822"); //un color random
+      interaction.editReply({
+         embeds: [embed]
+      }); //y que mande el embed
+   }
 }
